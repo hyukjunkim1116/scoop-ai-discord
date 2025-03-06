3145072a2d6614300b78194f5571111735e73fde
hyukjunkim1116 <nmkk1234@naver.com>
2025-03-06 16:18:40 +0900

    change image urls

--- v2/image/image_url.py
+++ v2/image/image_url.py
@@ -13,11 +13,11 @@ from enum import Enum
 
 
 class ImageURL(Enum):
-    KING_KONG = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-2g89ZDcaFj99ZvEkVjKHjQ07/user-aqKfjdrelqsWLJfdG6RAEFsP/img-TOY0rpWsUxDyE4Lso3364C8T.png?st=2025-03-06T06%3A09%3A49Z&se=2025-03-06T08%3A09%3A49Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-06T03%3A56%3A51Z&ske=2025-03-07T03%3A56%3A51Z&sks=b&skv=2024-08-04&sig=1dav580gDvTG0ylRl/BmkwgD1mMP%2BHVMU3JYeCsRMO8%3D"
-    DAMI = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-2g89ZDcaFj99ZvEkVjKHjQ07/user-aqKfjdrelqsWLJfdG6RAEFsP/img-kNm0wt1dsmeHOvcPPWGJpUq4.png?st=2025-03-04T06%3A43%3A26Z&se=2025-03-04T08%3A43%3A26Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-04T02%3A16%3A01Z&ske=2025-03-05T02%3A16%3A01Z&sks=b&skv=2024-08-04&sig=XtbyE%2BOmgoJNxKhZxsmckOWBqFvXCnX5HxF4EOC3FOY%3D"
-    CUTIE_YENA = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-2g89ZDcaFj99ZvEkVjKHjQ07/user-aqKfjdrelqsWLJfdG6RAEFsP/img-TrOwj6YzepG02ciKyKuYDqJ2.png?st=2025-03-04T06%3A43%3A43Z&se=2025-03-04T08%3A43%3A43Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-04T02%3A10%3A28Z&ske=2025-03-05T02%3A10%3A28Z&sks=b&skv=2024-08-04&sig=wv4fx%2B/v3Kx74ZOOSfs6RyThMF5NDv/Kafmvr5g7JP0%3D"
-    HEUKJIN = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-2g89ZDcaFj99ZvEkVjKHjQ07/user-aqKfjdrelqsWLJfdG6RAEFsP/img-DZ27NQxdq5IF5OrOeTdlFdCg.png?st=2025-03-04T06%3A44%3A09Z&se=2025-03-04T08%3A44%3A09Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-03T20%3A49%3A01Z&ske=2025-03-04T20%3A49%3A01Z&sks=b&skv=2024-08-04&sig=4IP27urjeaun3WX8Eg1lVoy0efS4NvJh7EcOTTe8KN0%3D"
-    VOID_RESONANCE = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-2g89ZDcaFj99ZvEkVjKHjQ07/user-aqKfjdrelqsWLJfdG6RAEFsP/img-eNfQ9QIbHMg4ViroKiii21ph.png?st=2025-03-04T06%3A44%3A36Z&se=2025-03-04T08%3A44%3A36Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-03-04T02%3A53%3A24Z&ske=2025-03-05T02%3A53%3A24Z&sks=b&skv=2024-08-04&sig=b3QxfC23ip9QNVZCO%2BchvcgaxoBbqPfg%2BqMJWgeUyLc%3D"
+    KING_KONG = "https://raw.githubusercontent.com/hyukjunkim1116/scoop-ai-discord/main/v2/image/image_files/kingkong.png"
+    DAMI = "https://raw.githubusercontent.com/hyukjunkim1116/scoop-ai-discord/main/v2/image/image_files/dami.png"
+    CUTIE_YENA = "https://raw.githubusercontent.com/hyukjunkim1116/scoop-ai-discord/main/v2/image/image_files/yena.png"
+    HEUKJIN = "https://raw.githubusercontent.com/hyukjunkim1116/scoop-ai-discord/main/v2/image/image_files/heukjin.png"
+    VOID_RESONANCE = "https://raw.githubusercontent.com/hyukjunkim1116/scoop-ai-discord/main/v2/image/image_files/resonance.png"
     CHARACTER_MAP = {
         "킹콩": KING_KONG,
         "다미": DAMI,
